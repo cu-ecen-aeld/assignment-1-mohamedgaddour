f40a758d4aa7d13e005727282d22a6934e9cea4a
mohamedgaddour <mohamedgaddour07@gmail.com>
2024-07-05 00:27:14 +0100

    assignement1

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,23 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+
+filesdir=$1
+searchstr=$2
+
+# checking if any of the arguments were not specified
+	if [ $# -ne 2 ]; then
+	echo "Error: Required two arguments: <path to directory on filesystem> <text string to be search within filesdir>"
+	exit 1
+	fi
 
 
+# checking if filesdir does not represent a directory on the filesystem
+	if [ ! -d "$filesdir" ]; then
+	echo "Error $filesdir is not a directory"
+	exit 1
+	fi
 
-if [ "$#" -ne 2 ]; then
-    echo "Illegal number of arguments"
-    echo "expected: <filesdir> <searchstr>"
-    exit 1
-fi
-FILESDIR=$1
-SEARCHSTR=$2
+# find the number of files in filesdir and its subdirectory
+total_files=$(find "$filesdir" -type f | wc -l)
 
-if [! -d $FILESDIR ]; then
-    echo "$FILESDIR does not represent a directory on the filesystem!"
-    exit 1   
-fi
-FILESNO=$( find $FILESDIR -type f | wc -l )
-MATCHINGLINES=$(grep -rnwl $FILESDIR -e $SEARCHSTR | wc -l )
+# find the number of files matching the search string
+total_matching_lines=$(grep -r "$searchstr" "$filesdir" 2>/dev/null | wc -l)
 
-echo "The number of files are $FILESNO , the number of matching lines are $MATCHINGLINES"
 
-echo "success"
-exit 0S
+echo "The number of files are $total_files and the number of matching lines are $total_matching_lines"
